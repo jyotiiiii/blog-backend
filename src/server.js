74a7b43d652273473a42e91c164d49a7081574ce
Jyoti <jyoti.contact@gmail.com>
2020-06-22 22:15:20 +0100

    add connection to database

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-// const articlesInfo = {
-//   'learn-react': {
-//     upvotes: 0,
-//     comments: [],
-//   },
-//   'learn-node': {
-//     upvotes: 0,
-//     comments: [],
-//   },
-//   'my-thoughts-on-resumes': {
-//     upvotes: 0,
-//     comments: [],
-//   },
-// };
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+    });
+    const db = client.db('my-blog');
+
+    const articleInfo = await db
+      .collection('articles')
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+});
 
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
@@ -27,7 +33,7 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   res
     .status(200)
     .send(
-      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
     );
 });
 
@@ -36,11 +42,8 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
   const articleName = req.params.name;
 
   articlesInfo[articleName].comments.push({ username, text });
+
   res.status(200).send(articlesInfo[articleName]);
 });
-
-// app.get('/hello', (req, res) => res.send('Hello!'));
-// app.get('/hello/:name', (req, res) => res.send(`Hello, ${req.params.name}!`));
-// app.post('/hello', (req, res) => res.send(`Hello, ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
