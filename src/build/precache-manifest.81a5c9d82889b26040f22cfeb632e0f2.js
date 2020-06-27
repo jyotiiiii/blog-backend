e121940ac86f1d1e952fd3dee6167ba59090b0d2
Jyoti <jyoti.contact@gmail.com>
2020-06-27 17:25:12 +0100

    add front-end build to src

---
+++ src/build/precache-manifest.81a5c9d82889b26040f22cfeb632e0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821db7d9b7ea3879ccb06b83c65fa6c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533d0cdff0e9d1d91a6b",
+    "url": "/static/css/main.ad3a872f.chunk.css"
+  },
+  {
+    "revision": "28159cac82941d65a4ce",
+    "url": "/static/js/2.211b7149.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.211b7149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533d0cdff0e9d1d91a6b",
+    "url": "/static/js/main.bf4951c4.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
